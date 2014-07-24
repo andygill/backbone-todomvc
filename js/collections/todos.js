0afd8cbeef61cbb11be74dbb33286318fa1de9bb
Andy Gill <andygill@ku.edu>
2014-07-24 00:05:28 -0500

    Fixing the max problem

--- js/collections/todos.js
+++ js/collections/todos.js
@@ -28,7 +28,8 @@ var app = app || {};
 		// We keep the Todos in sequential order, despite being saved by unordered
 		// GUID in the database. This generates the next order number for new items.
 		nextOrder: function () {
-			return this.length ? this.last().get('order') + 1 : 1;
+			var last = _.max(this.toArray(),function(e) {return e.get('order'); })
+			return this.length ? last.get('order') + 1 : 1;
 		},
 
 		// Todos are sorted by their original insertion order.
